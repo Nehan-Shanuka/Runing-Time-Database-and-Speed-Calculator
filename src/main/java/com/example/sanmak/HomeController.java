2aa42a209e257eaa6050d932c94e6291f01249b7
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-09-23 01:56:24 +0530

    Add an action to show input stage

--- src/main/java/com/example/sanmak/HomeController.java
+++ src/main/java/com/example/sanmak/HomeController.java
@@ -1,4 +1,29 @@
 package com.example.sanmak;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomeController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    protected void inputStage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("input-page.fxml"));
+        scene = new Scene(root);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Input a Runner");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
