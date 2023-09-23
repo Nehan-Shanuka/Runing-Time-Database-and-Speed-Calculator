6fb402dc87965d533bd2389c402eaffc61847422
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-09-23 12:35:58 +0530

    Connect the local database to the poject and inisialize function that want to add data into the database

--- src/main/java/com/example/sanmak/Main.java
+++ src/main/java/com/example/sanmak/Main.java
@@ -1,15 +1,66 @@
 package com.example.sanmak;
 
+import java.sql.Statement;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
-    private String name;
-    private int radius;
-    private String startTime;
-    private String endTime;
-    private int numLaps;
+    private static String name;
+    private static double radius;
+    private static String start_time;
+    private static String end_time;
+    private static int laps;
 
-    public static void main(String[] args) {
+    public Main(String name, double radius, String start_time, String end_time, int laps) {
+        Main.name = name;
+        Main.radius = radius;
+        Main.start_time =start_time;
+        Main.end_time = end_time;
+        Main.laps = laps;
+    }
 
+    public static void inputData() {
+
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        LocalTime startTime = null;
+        LocalTime endTime = null;
+
+        try {
+            startTime = LocalTime.parse(start_time, timeFormatter);
+
+            // You can now use the startTime as needed
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            endTime = LocalTime.parse(end_time, timeFormatter);
+
+            // You can now use the startTime as needed
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    try {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sanmac_interview",
+                "root", "RICHmond@?12");
+
+        Statement statement = connection.createStatement();
+
+        String sqlQuery = "INSERT INTO runners (name, radius, start_time, end_time, lap_count)" +
+                "VALUES ('"+name+"', '"+radius+"','"+startTime+"','"+endTime+"','"+laps+"')";
+
+        statement.execute(sqlQuery);
+
+        System.out.println("Added Successfully!!!");
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
 
     }
 
@@ -21,35 +72,35 @@ public class Main {
         this.name = name;
     }
 
-    public int getRadius() {
+    public double getRadius() {
         return radius;
     }
 
-    public void setRadius(int radius) {
+    public void setRadius(double radius) {
         this.radius = radius;
     }
 
     public String getStartTime() {
-        return startTime;
+        return start_time;
     }
 
     public void setStartTime(String startTime) {
-        this.startTime = startTime;
+        this.start_time = startTime;
     }
 
     public String getEndTime() {
-        return endTime;
+        return end_time;
     }
 
     public void setEndTime(String endTime) {
-        this.endTime = endTime;
+        this.end_time = endTime;
     }
 
     public int getNumLaps() {
-        return numLaps;
+        return laps;
     }
 
     public void setNumLaps(int numLaps) {
-        this.numLaps = numLaps;
+        this.laps = numLaps;
     }
 }
