da679b8cf5970c53bdc71d60e1f7de955579c06a
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-09-23 08:25:27 +0530

    Add the Main class to implement the functions

---
+++ src/main/java/com/example/sanmak/Main.java
@@ -0,0 +1,55 @@
+package com.example.sanmak;
+
+public class Main {
+
+    private String name;
+    private int radius;
+    private String startTime;
+    private String endTime;
+    private int numLaps;
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getNumLaps() {
+        return numLaps;
+    }
+
+    public void setNumLaps(int numLaps) {
+        this.numLaps = numLaps;
+    }
+}
