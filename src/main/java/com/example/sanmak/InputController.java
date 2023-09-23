5674825939b883dd6d4b8cda4091581599487248
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-09-23 12:40:35 +0530

    Implement the action for the ADD btn in input-page

--- src/main/java/com/example/sanmak/InputController.java
+++ src/main/java/com/example/sanmak/InputController.java
@@ -1,6 +1,33 @@
 package com.example.sanmak;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class InputController {
 
+    @FXML
+    private TextField nameText;
+    @FXML
+    private TextField radiusText;
+    @FXML
+    private TextField startTimeText;
+    @FXML
+    private TextField endTimeText;
+    @FXML
+    private TextField numLapText;
 
+    @FXML
+    protected void addTheRunner() {
+
+        String name = nameText.getText();
+        double radius = Double.parseDouble(radiusText.getText());
+        String startTime = startTimeText.getText();
+        String endTime = endTimeText.getText();
+        int numLaps = Integer.parseInt(numLapText.getText());
+
+        Main main = new Main(name, radius, startTime, endTime, numLaps);
+
+        Main.inputData();
+    }
 }
