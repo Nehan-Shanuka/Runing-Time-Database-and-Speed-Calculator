6db1f5481759c499f5c16ddc0c829e5fae5ad39a
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-09-24 08:06:24 +0530

    Add controller class to the new batabase-page.fxml scene

---
+++ src/main/java/com/example/sanmak/DatabaseController.java
@@ -0,0 +1,13 @@
+package com.example.sanmak;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DatabaseController {
+}
