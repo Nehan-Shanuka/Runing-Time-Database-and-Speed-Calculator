c25818a48f67837ac1d23e28055fc6b7233b78ac
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-09-24 08:10:58 +0530

    Add a new method to update data of runners

--- src/main/java/com/example/sanmak/Main.java
+++ src/main/java/com/example/sanmak/Main.java
@@ -1,10 +1,9 @@
 package com.example.sanmak;
 
-import java.sql.Statement;
+import java.sql.*;
+import java.time.Duration;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 public class Main {
 
@@ -13,6 +12,9 @@ public class Main {
     private static String start_time;
     private static String end_time;
     private static int laps;
+    private static DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private static LocalTime startTime = null;
+    private static LocalTime endTime = null;
 
     public Main(String name, double radius, String start_time, String end_time, int laps) {
         Main.name = name;
@@ -23,11 +25,6 @@ public class Main {
     }
 
     public static void inputData() {
-
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-
-        LocalTime startTime = null;
-        LocalTime endTime = null;
 
         try {
             startTime = LocalTime.parse(start_time, timeFormatter);
@@ -45,23 +42,83 @@ public class Main {
             e.printStackTrace();
         }
 
-    try {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sanmac_interview",
-                "root", "RICHmond@?12");
+        Duration timeDifference = Duration.between(startTime, endTime);
 
-        Statement statement = connection.createStatement();
+        float runTimeInSecs = timeDifference.toSeconds();
 
-        String sqlQuery = "INSERT INTO runners (name, radius, start_time, end_time, lap_count)" +
-                "VALUES ('"+name+"', '"+radius+"','"+startTime+"','"+endTime+"','"+laps+"')";
+        float runTimeInHours = runTimeInSecs / (60*60);
+        System.out.println(runTimeInHours);
 
-        statement.execute(sqlQuery);
+        float trackLength = (float) ((float) 2 * Math.PI * radius * laps) / 1000;
+        System.out.println(trackLength);
 
-        System.out.println("Added Successfully!!!");
+        float speed = trackLength / runTimeInHours;
+        System.out.println(speed);
 
-    } catch (Exception e) {
-        e.printStackTrace();
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sanmac_interview",
+                    "root", "RICHmond@?12");
+
+            Statement statement = connection.createStatement();
+
+            String sqlQuery = "INSERT INTO runners (name, radius, start_time, end_time, lap_count, speed, duration)" +
+                    "VALUES ('" + name + "', '" + radius + "','" + startTime + "','" + endTime + "','" + laps + "'," +
+                    "'"+speed+"','"+runTimeInSecs+"')";
+
+            statement.execute(sqlQuery);
+
+            System.out.println("Added Successfully!!!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
+    public void updateData() {
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sanmac_interview",
+                    "root", "RICHmond@?12");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM runners");
+
+            while (resultSet.next()) {
+                radius = resultSet.getFloat("radius");
+                startTime = LocalTime.parse(resultSet.getString("start_time"), timeFormatter);
+                endTime = LocalTime.parse(resultSet.getString("end_time"), timeFormatter);
+                laps = resultSet.getInt("lap_count");
+
+                Duration timeDifference = Duration.between(startTime, endTime);
+
+                float runTimeInSecs = timeDifference.toSeconds();
+
+                float runTimeInHours = runTimeInSecs / (60 * 60);
+
+                float trackLength = (float) ((float) 2 * Math.PI * radius * laps) / 1000;
+
+                float speed = trackLength / runTimeInHours;
+
+                String updateSQL = "UPDATE runners SET speed = ?, duration = ? WHERE name = ?";
+
+                try {
+                    PreparedStatement preparedStatement = connection.prepareStatement(updateSQL);
+
+                    preparedStatement.setFloat(1, speed);
+                    preparedStatement.setFloat(2, runTimeInSecs);
+                    preparedStatement.setString(3, resultSet.getString("name"));
+
+                    preparedStatement.executeUpdate();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public String getName() {
