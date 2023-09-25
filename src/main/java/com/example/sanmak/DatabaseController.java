6b960a54ebc00a1008f320435ca82f6c3d3e0c8c
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-09-25 11:28:18 +0530

    Implement methods to get and show the data

--- src/main/java/com/example/sanmak/DatabaseController.java
+++ src/main/java/com/example/sanmak/DatabaseController.java
@@ -1,13 +1,102 @@
 package com.example.sanmak;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.sql.*;
 import java.util.ResourceBundle;
 
-public class DatabaseController {
+public class DatabaseController extends HomeController implements Initializable {
+
+    @FXML
+    private TableView<Runner> runnersTable;
+    @FXML
+    private TableColumn<Runner, String> nameCol;
+    @FXML
+    private TableColumn<Runner, Float> speedCol;
+    @FXML
+    private TableColumn<Runner, Double> radiusCol;
+    @FXML
+    private TableColumn<Runner, String> startTimeCol;
+    @FXML
+    private TableColumn<Runner, String> endTimeCol;
+    @FXML
+    private TableColumn<Runner, Float> durationCol;
+    @FXML
+    private TableColumn<Runner, Integer> lapCol;
+
+    @FXML
+    private Button btnBack;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            loadData();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sanmac_interview",
+                    "root", "RICHmond@?12");
+
+            return connection;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ObservableList<Runner> getRunnersList() {
+        ObservableList<Runner> runnerList = FXCollections.observableArrayList();
+        Connection connection = getConnection();
+        String query = "SELECT * FROM runners";
+        Statement statement;
+        ResultSet resultSet;
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+            Runner runner;
+            while (resultSet.next()) {
+                runner = new Runner(resultSet.getString("name"), resultSet.getFloat("speed"),
+                        resultSet.getDouble("radius"), resultSet.getString("start_time"),
+                        resultSet.getString("end_time"), resultSet.getFloat("duration"),
+                        resultSet.getInt("lap_count"));
+
+                runnerList.add(runner);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return runnerList;
+    }
+
+    private void loadData() throws SQLException {
+
+        ObservableList<Runner> list = getRunnersList();
+
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        speedCol.setCellValueFactory(new PropertyValueFactory<>("speed"));
+        radiusCol.setCellValueFactory(new PropertyValueFactory<>("radius"));
+        startTimeCol.setCellValueFactory(new PropertyValueFactory<>("startTime"));
+        endTimeCol.setCellValueFactory(new PropertyValueFactory<>("endTime"));
+        durationCol.setCellValueFactory(new PropertyValueFactory<>("duration"));
+        lapCol.setCellValueFactory(new PropertyValueFactory<>("lap"));
+
+        runnersTable.setItems(list);
+    }
 }
