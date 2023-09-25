4bf5809bd66c6492df371b5fb06573dfe80860ad
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-09-25 12:32:44 +0530

    Change the width and height of the start scene

--- src/main/java/com/example/sanmak/RuntimeApplication.java
+++ src/main/java/com/example/sanmak/RuntimeApplication.java
@@ -16,7 +16,7 @@ public class RuntimeApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(RuntimeApplication.class.getResource("home-page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         primaryStage.setTitle("Welcome to Runtime Application");
         primaryStage.setScene(scene);
         primaryStage.show();
