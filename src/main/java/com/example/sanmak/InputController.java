3fa3af98ff0f661c143e2ed79ff6a3fff7fba143
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-09-25 17:28:43 +0530

    Replace the input method in the InputController class

--- src/main/java/com/example/sanmak/InputController.java
+++ src/main/java/com/example/sanmak/InputController.java
@@ -3,6 +3,13 @@ package com.example.sanmak;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class InputController extends HomeController {
 
     @FXML
@@ -27,6 +34,73 @@ public class InputController extends HomeController {
 
         Runner runner = new Runner(name, 0, radius, startTime, endTime, 0, numLaps);
 
-        runner.inputData();
+        inputData(runner);
+    }
+
+    private Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sanmac_interview",
+                    "root", "RICHmond@?12");
+
+            return connection;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    protected void inputData(Runner runner) {
+
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        LocalTime start_time = null;
+        LocalTime end_time = null;
+
+        try {
+            start_time = LocalTime.parse(runner.getStartTime(), timeFormatter);
+
+            // You can now use the startTime as needed
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            end_time = LocalTime.parse(runner.getEndTime(), timeFormatter);
+
+            // You can now use the startTime as needed
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Duration timeDifference = Duration.between(start_time, end_time);
+
+        float duration = timeDifference.toSeconds();
+        runner.setDuration(duration);
+
+        float runTimeInHours = duration / (60*60);
+
+        float trackLength = (float) ((float) 2 * Math.PI * runner.getRadius() * runner.getLap()) / 1000;
+
+        float speed = trackLength / runTimeInHours;
+        runner.setSpeed(speed);
+
+        try {
+            Connection connection = getConnection();
+
+            Statement statement = connection.createStatement();
+
+            String sqlQuery = "INSERT INTO runners (name, speed, radius, start_time, end_time, duration, lap_count)" +
+                    "VALUES ('" + runner.getName() + "','" + speed + "','" + runner.getRadius() + "','" + runner.getStartTime() +
+                    "','" + runner.getEndTime() + "','" + duration + "','"+ runner.getLap() + "')";
+
+            statement.execute(sqlQuery);
+
+            System.out.println("Added Successfully!!!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/com/example/sanmak/Runner.java
+++ src/main/java/com/example/sanmak/Runner.java
@@ -1,12 +1,5 @@
 package com.example.sanmak;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.time.Duration;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-
 public class Runner {
 
     private String name;
@@ -26,71 +19,6 @@ public class Runner {
         this.endTime = endTime;
         this.duration = duration;
         this.lap = lap;
-    }
-
-    private Connection getConnection() {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sanmac_interview",
-                    "root", "RICHmond@?12");
-
-            return connection;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    protected void inputData() {
-
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-
-        LocalTime start_time = null;
-        LocalTime end_time = null;
-
-        try {
-            start_time = LocalTime.parse(startTime, timeFormatter);
-
-            // You can now use the startTime as needed
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
-            end_time = LocalTime.parse(endTime, timeFormatter);
-
-            // You can now use the startTime as needed
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Duration timeDifference = Duration.between(start_time, end_time);
-
-        duration = timeDifference.toSeconds();
-
-        float runTimeInHours = duration / (60*60);
-
-        float trackLength = (float) ((float) 2 * Math.PI * radius * lap) / 1000;
-
-        speed = trackLength / runTimeInHours;
-
-        try {
-            Connection connection = getConnection();
-
-            Statement statement = connection.createStatement();
-
-            String sqlQuery = "INSERT INTO runners (name, speed, radius, start_time, end_time, duration, lap_count)" +
-                    "VALUES ('" + name + "','" + speed + "','" + radius + "','" + startTime + "','" + endTime + "','"
-                    + duration + "','"+ lap + "')";
-
-            statement.execute(sqlQuery);
-
-            System.out.println("Added Successfully!!!");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
 
     public String getName() {
